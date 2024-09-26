4327c57a23d8cdd5132bf6a16927d2eeec411f8d
Akshit soni <akshitsoni@Akshits-Macbook-Air.local>
2024-09-26 12:04:57 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -3,13 +3,7 @@ import sys
 # import pyparsing - available if you need it!
 # import lark - available if you need it!
 
-class Pattern:
-    DIGIT = "\d"
-    ALNUM = "\w"
-# def combined_patterns(input_line, pattern):
-
-
-def match_pattern(input_line, pattern):    
+def match_pattern(input_line, pattern):
     if not pattern:
         return True
     if not input_line:
@@ -18,35 +12,36 @@ def match_pattern(input_line, pattern):
         return match_pattern(input_line, pattern[1:])
     elif pattern[0] == input_line[0]:
         return match_pattern(input_line[1:], pattern[1:])
-    elif pattern[:2] == Pattern.DIGIT:
-        for i in range(len(input_line)):
-            if input_line[i].isdigit():
-                return match_pattern(input_line[i:], pattern[2:])
-        else:
-            return False
-    elif pattern[:2] == Pattern.ALNUM:
-        if input_line[0].isalnum():
-            return match_pattern(input_line[1:], pattern[2:])
-        else:
-            return False
-    elif pattern[0] == "[" and pattern[-1] == "]":
+    elif pattern[:2] == "\d":
+        return (
+            input_line[0].isdigit() and match_pattern(input_line[1:], pattern[2:])
+        ) or match_pattern(input_line[1:], pattern)
+    elif pattern[:2] == "\w":
+        return (
+            (input_line[0].isalnum() or input_line[0] == "_")
+            and match_pattern(input_line[1:], pattern[2:])
+        ) or match_pattern(input_line[1:], pattern)
+    elif pattern[0] == "[":
+        negate = False
         if pattern[1] == "^":
-            chrs = list(pattern[2:-1])
-            for c in chrs:
-                if c in input_line:
-                    return False
-            return True
-        chrs = list(pattern[1:-1])
-        for c in chrs:
+            negate = True
+        idx = pattern.find("]")
+        if idx == -1:
+            return False
+        chars = pattern[1:idx]
+        if negate:
+            chars = pattern[2:idx]
+        for c in chars:
             if c in input_line:
-                return True
-        return False
+                return True if (not negate) else False
+        return False if (not negate) else True
     else:
-        return match_pattern(input_line[1:], pattern)
+        return False
 
 def main():
     pattern = sys.argv[2]
     input_line = sys.stdin.read()
+    input_line = input_line.strip()
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
