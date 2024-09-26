6c4d46502624cedbebe6126f3848abf1b15d33c7
Akshit soni <akshitsoni@Akshits-Macbook-Air.local>
2024-09-26 14:14:39 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,85 +1,69 @@
 import sys
 
-# import pyparsing - available if you need it!
+# import pyparsing
 # import lark - available if you need it!
 
-class Pattern:
-    DIGIT = "\d"
-    ALNUM = "\w"
+def check_next(input_line, pattern, offset_next):
+    if (len(pattern) > offset_next + 1) and pattern[offset_next] == "+":
+        return match_local(input_line, pattern[offset_next + 1 :]) or match_local(
+            input_line, pattern
+        )
+    return match_local(input_line, pattern[offset_next:])
 
-# def combined_patterns(input_line, pattern):
-
-def match_pattern(input_line, pattern):
-    if len(input_line) == 0 and len(pattern) == 0:
-        return True
-    if not pattern:
-        return True
-    if not input_line:
-        return False
-    if pattern[0] == "^":
-        pattern = pattern[1:]
-        while len(pattern) > 0 and len(input_line) > 0:
-            if pattern[0] == input_line[0]:
-                pattern = pattern[1:]
-                input_line = input_line[1:]
-            else:
-                return False
-        return match_pattern(input_line, pattern)
-    if pattern[-1] == "$":
-        pattern = pattern[:-1]
-        while len(pattern) > 0 and len(input_line) > 0:
-            if pattern[-1] == input_line[-1]:
-                pattern = pattern[:-1]
-                input_line = input_line[:-1]
-            else:
-                return False
-        return match_pattern(input_line, pattern)
-    if pattern[0] == input_line[0]:
-        if len(pattern) > 1:
-            if pattern[1] == "+":
-                return match_pattern(input_line[1:], pattern[2:]) or match_pattern(
-                    input_line[1:], pattern
-                )
-            elif pattern[1] == "?":
-                return match_pattern(input_line[1:], pattern[2:]) or match_pattern(
-                    input_line, pattern[1:]
-                )
-        return match_pattern(input_line[1:], pattern[1:])
-    elif pattern[0] != input_line[0] and len(pattern) > 1 and pattern[1] == "?":
-        if pattern[1] == "?":
-            return match_pattern(input_line, pattern[2:])
-        return False
-    elif pattern[0] == ".":
-        return match_pattern(input_line[1:], pattern[1:])
-    elif pattern[:2] == Pattern.DIGIT:
-        for i in range(len(input_line)):
-            if input_line[i].isdigit():
-                return match_pattern(input_line[i:], pattern[2:])
-        else:
-            return False
-    elif pattern[:2] == Pattern.ALNUM:
-        if input_line[0].isalnum():
-            return match_pattern(input_line[1:], pattern[2:])
-        else:
-            return False
-    elif pattern[0] == "[" and pattern[-1] == "]":
-        if pattern[1] == "^":
-            chrs = list(pattern[2:-1])
-            for c in chrs:
-                if c in input_line:
-                    return False
-            return True
-        chrs = list(pattern[1:-1])
-        for c in chrs:
-            if c in input_line:
-                return True
-        return False
+def check(predicate: bool, input_line: str, pattern: str, offset: int):
+    is_optional = len(pattern) > offset + 1 and pattern[offset] == "?"
+    if predicate:
+        return check_next(
+            input_line[1:], pattern, offset + 1 if is_optional else offset
+        )
     else:
-        return match_pattern(input_line[1:], pattern)
+        return check_next(input_line, pattern, offset + 1) if is_optional else False
+
+
+def match_local(input_line: str, pattern: str):
+    if len(pattern) == 0:
+        return True
+    if len(input_line) == 0:
+        return pattern == "$"
+    if pattern.startswith("."):
+        return check(True, input_line, pattern, 1)
+    if pattern.startswith("\d"):
+        return check(input_line[0].isdigit(), input_line, pattern, 2)
+    elif pattern.startswith("\w"):
+        return check(input_line[0].isalnum(), input_line, pattern, 2)
+    elif pattern.startswith("[^") and "]" in pattern:
+        chars = pattern[1 : pattern.index("]")]
+        return check(
+            input_line[0] not in chars, input_line, pattern, pattern.index("]") + 1
+        )
+    elif pattern.startswith("[") and "]" in pattern:
+        chars = pattern[1 : pattern.index("]")]
+        return check(
+            input_line[0] in chars, input_line, pattern, pattern.index("]") + 1
+        )
+    elif pattern.startswith("(") and ")" in pattern:
+        first, second = pattern[1 : pattern.index(")")].split("|")
+        pattern_truncated = pattern[pattern.index(")") + 1 :]
+        return match_local(input_line, first + pattern_truncated) or match_local(
+            input_line, second + pattern_truncated
+        )
+    else:
+        return check(pattern[0] == input_line[0], input_line, pattern, 1)
+def match_pattern(input_line: str, pattern: str):
+    if pattern[0] == "^":
+        return match_local(input_line, pattern[1:])
+    if match_local(input_line, pattern):
+        return True
+    else:
+        truncated = input_line[1:]
+        if len(truncated) == 0:
+            return pattern == "$"
+        return match_pattern(truncated, pattern)
+
 
 def main():
     pattern = sys.argv[2]
-    input_line = sys.stdin.read()
+    input_line = sys.stdin.read().splitlines()[0]
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
@@ -87,6 +71,7 @@ def main():
         exit(0)
     else:
         exit(1)
+
 
 if __name__ == "__main__":
     main()
