98368de089d9edb78ac268e6214f5a7dd74bf5c1
Akshit soni <akshitsoni@Akshits-Macbook-Air.local>
2024-09-26 11:24:31 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 import sys
 
-# Define constants for patterns
-DIGIT = "\\d"
-ALNUM = "\\w"
+# import pyparsing - available if you need it!
+# import lark - available if you need it!
+class Pattern:
+    DIGIT = "\\d"
+    ALNUM = "\\w"
 
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
-    elif pattern == DIGIT:
+    elif pattern == "\\d":
         return any(c.isdigit() for c in input_line)
     
     if len(input_line) == 0 and len(pattern) == 0:
@@ -18,18 +20,19 @@ def match_pattern(input_line, pattern):
         return False
     if pattern[0] == input_line[0]:
         return match_pattern(input_line[1:], pattern[1:])
-    elif pattern[:2] == DIGIT:
+    elif pattern[:2] == Pattern.DIGIT:
         for i in range(len(input_line)):
             if input_line[i].isdigit():
                 return match_pattern(input_line[i:], pattern[2:])
         else:
             return False
-    elif pattern[:2] == ALNUM:
+    elif pattern[:2] == Pattern.ALNUM:
         if input_line[0].isalnum():
             return match_pattern(input_line[1:], pattern[2:])
         else:
             return False
-    elif pattern == ALNUM:
+    
+    elif pattern == "\\w":
         return any(c.isalnum() for c in input_line)
     elif pattern[0] == "[" and pattern[-1] == "]":
         if pattern[1] == r"^":
@@ -51,6 +54,7 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
+    
     if match_pattern(input_line, pattern):
         exit(0)
     else:
