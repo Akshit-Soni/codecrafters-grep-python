5340948ece407dd02d0b7609f9d972a5cf9a3691
Akshit soni <akshitsoni@Akshits-Macbook-Air.local>
2024-09-26 11:12:16 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -18,13 +18,13 @@ def match_pattern(input_line, pattern):
         return False
     if pattern[0] == input_line[0]:
         return match_pattern(input_line[1:], pattern[1:])
-    elif pattern[:2] == Pattern.DIGIT:
+    elif pattern[:2] == pattern.DIGIT:
         for i in range(len(input_line)):
             if input_line[i].isdigit():
                 return match_pattern(input_line[i:], pattern[2:])
         else:
             return False
-    elif pattern[:2] == Pattern.ALNUM:
+    elif pattern[:2] == pattern.ALNUM:
         if input_line[0].isalnum():
             return match_pattern(input_line[1:], pattern[2:])
         else:
