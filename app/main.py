117f5a5f54896096ef7784f360c05dda0f4692d4
Akshit soni <akshitsoni@Akshits-Macbook-Air.local>
2024-09-26 11:11:23 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -9,6 +9,27 @@ def match_pattern(input_line, pattern):
         return pattern in input_line
     elif pattern == "\\d":
         return any(c.isdigit() for c in input_line)
+    
+    if len(input_line) == 0 and len(pattern) == 0:
+        return True
+    if not pattern:
+        return True
+    if not input_line:
+        return False
+    if pattern[0] == input_line[0]:
+        return match_pattern(input_line[1:], pattern[1:])
+    elif pattern[:2] == Pattern.DIGIT:
+        for i in range(len(input_line)):
+            if input_line[i].isdigit():
+                return match_pattern(input_line[i:], pattern[2:])
+        else:
+            return False
+    elif pattern[:2] == Pattern.ALNUM:
+        if input_line[0].isalnum():
+            return match_pattern(input_line[1:], pattern[2:])
+        else:
+            return False
+    
     elif pattern == "\\w":
         return any(c.isalnum() for c in input_line)
     elif pattern[0] == "[" and pattern[-1] == "]":
@@ -17,6 +38,7 @@ def match_pattern(input_line, pattern):
         return any(char in pattern[1:-1] for char in input_line)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
+        return match_pattern(input_line[1:], pattern)
 
 
 def main():
